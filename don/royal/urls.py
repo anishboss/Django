268fdd91cd8380b72837dc68c1725fda5029f2ec
anishboss <anishbudhathoki@gmai.com>
2019-12-27 09:21:13 +0545

    12/27/2019

--- don/royal/urls.py
+++ don/royal/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns=[
     path('',views.index,name="index"),
-    path('list/',views.list,name="list")
+    path('list/',views.list,name="list"),
+    path('delete/<int:id>/',views.employee_delete,name = 'employee_delete'),
+    path('<int:id>/',views.index, name= 'employee_update')
     ]
--- don/royal/views.py
+++ don/royal/views.py
@@ -4,18 +4,29 @@ from django.http import HttpResponse
 from . forms import EmployeeForm
 
 
-def index(request):
+def index(request,id=0):
 	if request.method == 'GET':
-		employee = EmployeeForm()
+		if id==0:
+			employee = EmployeeForm()
+		else:
+			data = Employee.objects.get(pk=id)
+			employee = EmployeeForm(instance=data)
 		return render(request,'index.html',{'employee':employee})
 	else:
-		employee = EmployeeForm(request.POST)
+		if id==0:
+			employee = EmployeeForm(request.POST)
+		else:
+			 data = Employee.objects.get(pk=id)
+			 employee = EmployeeForm(request.POST,instance=data)
 		if employee.is_valid():
 			employee.save()
-		return redirect('/')
+		return redirect('/list')
 
 def list(request):
 	employee = Employee.objects.all()
 	return render(request,'list.html',{'employees':employee})
 
-	
+def employee_delete(request,id=0):
+	employee = Employee.objects.get(pk=id)
+	employee.delete()
+	return redirect('list')
